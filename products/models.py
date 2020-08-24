42b5c24f057157faa08463766516e664256ceb5b
Parrot Live user <user@localhost.localdomain>
2020-08-24 14:02:10 +0000

    under dev

--- products/models.py
+++ products/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class product(models.Model):
+    name =models.CharField(max_length=30)
+    price=models.FloatField(max_length=20)
+    image=models.CharField(max_length=8000)
+
--- products/urls.py
+++ products/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("",views.index,name="home")
+    path("",views.index,name="home"),
+    path("contact",views.contact),
     
 ]
