6f8b8890885ab0e4d74d741361db52d9a7abc0d8
Parrot Live user <user@localhost.localdomain>
2020-08-25 11:36:34 +0000

    admin.py added some setting

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from .models import product
-admin.site.register(product)
+# admin.site.register(product)
 
 # Register your models here.
+class productadmin(admin.ModelAdmin):
+    list_display=('name','price','image')
+admin.site.register(product,productadmin)
