b7178f6dbeb2227edbff48e8732ed3257619d01b
Parrot Live user <user@localhost.localdomain>
2020-08-25 12:34:20 +0000

    admin.py added some setting

--- products/urls.py
+++ products/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("",views.index,name="home"),
+    path("",views.index),
     path("contact",views.contact),
     
 ]
--- products/views.py
+++ products/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import product
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello");
+    # return HttpResponse("Hello");
+    products=product.object.all()
+    return render(request,'index.html',{"products":products})
 def contact(request):
     return HttpResponse("contact")
